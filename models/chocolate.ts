148ee6a1903b50f89c3953d28218097969af7038
Haruka Ogino <harukariq@gmail.com>
2024-10-12 11:41:14 +1300

    debug model

--- models/chocolate.ts
+++ models/chocolate.ts
@@ -2,7 +2,7 @@ export interface Position {
   brand: string
   image_url: string
   description: string
-  uses_ethically_grown_cocoa: true
+  uses_ethically_grown_cocoa: boolean
   location: Location
 }
 
