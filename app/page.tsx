3d8d1cad91ee199c96a5041666f3e104fe317653
Haruka Ogino <harukariq@gmail.com>
2024-10-12 11:41:31 +1300

    render real data

--- app/page.tsx
+++ app/page.tsx
@@ -9,36 +9,9 @@ import {
   InfoWindow,
 } from '@vis.gl/react-google-maps'
 import { Position } from '@/models/chocolate'
+import { chocolate } from './chocolate'
 
 export default function Intro() {
-  const positions: Position[] = [
-    {
-      brand: 'Lindt',
-      image_url: 'https://example.com/lindt.jpg',
-      description:
-        'Lindt is a Swiss chocolate company known for its fine, high-quality chocolate products.',
-      uses_ethically_grown_cocoa: true,
-      location: {
-        lat: 47.3769,
-        lng: 8.5417,
-        country: 'Switzerland',
-        city: 'Zurich',
-      },
-    },
-    {
-      brand: 'Valrhona',
-      image_url: 'https://example.com/valrhona.jpg',
-      description:
-        'Valrhona is a French chocolate maker, specializing in premium chocolate for gourmet chefs.',
-      uses_ethically_grown_cocoa: true,
-      location: {
-        lat: 45.918,
-        lng: 4.7005,
-        country: 'France',
-        city: "Tain-l'Hermitage",
-      },
-    },
-  ]
   const [open, setOpen] = useState(false)
 
   if (
@@ -50,18 +23,15 @@ export default function Intro() {
     )
   }
 
-  const markerImage =
-    'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <div style={{ height: '100vh', width: '100%' }}>
         <Map
           defaultZoom={9}
-          defaultCenter={positions[0].location}
+          defaultCenter={chocolate[0].location}
           mapId={process.env.NEXT_PUBLIC_MAP_ID}
         >
-          {positions.map((position: Position) => (
+          {chocolate.map((position: Position) => (
             <>
               <AdvancedMarker
                 position={position.location}
